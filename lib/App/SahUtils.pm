8a304d4789f10b867f260d614b375daf0091e383
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-03 16:57:15 +0700

    Initial commit

---
+++ lib/App/SahUtils.pm
@@ -0,0 +1,23 @@
+package App::SahUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Collection of CLI utilities for Sah and Data::Sah
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+L<Sah> and L<Data::Sah>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Data::Sah>
+
+=cut
